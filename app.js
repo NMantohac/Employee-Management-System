e32ffb5b7117755a88b488c71685cbea1b7d6341
Norberto Mantohac <nmantohac82098@gmail.com>
2020-04-23 23:39:53 -0700

    created sql-query.js and exported it to app.js with initial functions, and completed the basic inquire.prompt and switch statements in app.js

--- app.js
+++ app.js
@@ -1,10 +1,100 @@
-const mysql = require("mysql2");
-const inquirer = require("inquirer");
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+const sqlQuery = require('./sql-query');
 
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: 'localhost',
     port: 3306,
-    user: "root",
-    password: "password",
-    database: "top_songsDB"
-}).promise();
+    user: 'root',
+    password: 'password',
+    database: 'employee_tracker_db'
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log('Connected!');
+  initialize();
+})
+
+const initialize = async () => {
+  try {
+    const { search } = await inquirer.prompt(
+      {
+        type: 'list',
+        name: 'search',
+        message: 'What would you like to do?',
+        choices: ['View All Employees', 
+                  'View All Employees By Department', 
+                  'View All Employees By Manager', 
+                  'Add Employee',
+                  'Remove Employee',
+                  'Update Employee Role',
+                  'Update Employee Manager',
+                  'View All Roles',
+                  'Add Role',
+                  'Remove Role',
+                  'View All Departments',
+                  'Add Department',
+                  'Remove Department',
+                  'View Total Utilized Budget by Department',
+                  'Exit'
+                ]
+      }
+    )
+  
+    console.log(search);
+    
+    switch (search) {
+      case 'View All Employees':
+        sqlQuery.viewEmployees();
+        break;
+      case 'View All Employees By Department':
+        sqlQuery.viewEmployeesByDepartment();
+        break;
+      case 'View All Employees By Manager':
+        sqlQuery.viewEmployeesByManager();
+        break;
+      case 'Add Employee':
+        sqlQuery.addEmployee();
+        break;
+      case 'Remove Employee':
+        sqlQuery.removeEmployee();
+        break;
+      case 'Update Employee Role':
+        sqlQuery.updateEmployeeRole();
+        break;
+      case 'Update Employee Manager':
+        sqlQuery.updateEmployeeManager();
+        break;
+      case 'View All Roles':
+        sqlQuery.viewRoles();
+        break;
+      case 'Add Role':
+        sqlQuery.addRole();
+        break;
+      case 'Remove Role':
+        sqlQuery.removeRole();
+        break;
+      case 'View All Departments':
+        sqlQuery.viewDepartments();
+        break;
+      case 'Add Department':
+        sqlQuery.addDepartment();
+        break;
+      case 'Remove Department':
+        sqlQuery.removeDepartment();
+        break;
+      case 'View Total Utilized Budget by Department':
+        sqlQuery.viewBudgetByDepartment();
+        break;
+      case 'Exit':
+        connection.end();
+        break;
+      default:
+        connection.end();
+    }
+  } catch (err) {
+    throw err;
+  }
+}
---
+++ sql-query.js
@@ -0,0 +1,74 @@
+const inquirer = require("inquirer");
+
+const viewEmployees = () => {
+  console.log('a');
+}
+
+const viewEmployeesByDepartment = () => {
+  console.log('b');
+}
+
+const viewEmployeesByManager = () => {
+  console.log('c');
+}
+  
+const addEmployee = () => {
+  console.log('d');
+}
+
+const removeEmployee = () => {
+  console.log('e');
+}
+
+const updateEmployeeRole = () => {
+  console.log('f');
+}
+
+const updateEmployeeManager = () => {
+  console.log('g');
+}
+
+const viewRoles = () => {
+  console.log('h');
+}
+
+const addRole = () => {
+  console.log('i');
+}
+
+const removeRole = () => {
+  console.log('j');
+}
+
+const viewDepartments = () => {
+  console.log('k');
+}
+
+const addDepartment = () => {
+  console.log('l');
+}
+
+const removeDepartment = () => {
+  console.log('m');
+}
+
+const viewBudgetByDepartment = () => {
+  console.log('n');
+}
+
+module.exports = {
+  viewEmployees,
+  viewEmployeesByDepartment,
+  viewEmployeesByManager,
+  addEmployee,
+  removeEmployee,
+  updateEmployeeRole,
+  updateEmployeeManager,
+  viewRoles,
+  addRole,
+  removeRole,
+  viewDepartments,
+  addDepartment,
+  removeDepartment,
+  viewBudgetByDepartment
+};
