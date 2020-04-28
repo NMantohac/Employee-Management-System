ea63ce682030190c290c1b0bf4e6f4983d368feb
Norberto Mantohac <nmantohac82098@gmail.com>
2020-04-27 18:55:38 -0700

    final local mysql database for seed.sql

--- models/seed.sql
+++ models/seed.sql
@@ -5,26 +5,23 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department (
-	id INTEGER AUTO_INCREMENT NOT NULL, 
-    name VARCHAR(30), 
-    PRIMARY KEY (id)
+	id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+    name VARCHAR(30)
 );
 
 CREATE TABLE role (
-	id INTEGER AUTO_INCREMENT NOT NULL,
+	id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL,
-    department_id INTEGER,
-    PRIMARY KEY (id)
+    salary INTEGER,
+    department_id INTEGER
 );
 
 CREATE TABLE employee (
-	id INTEGER AUTO_INCREMENT NOT NULL,
+	id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER,
-    manager_id INTEGER NULL,
-    PRIMARY KEY (id)
+    manager_id INTEGER NULL
 );
 
 INSERT INTO department (name)
